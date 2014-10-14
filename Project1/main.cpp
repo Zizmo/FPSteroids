a8f4310f1680a3a46c0ad546b7824ba32084b7dc
Zizmo <kbrvrd@gmail.com>
2014-10-14 14:34:45 -0400

    openGL setup
    Program runs-- says "glew32.dll" is missing. It's like, right there,
    though...

---
+++ Project1/main.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main()
+{
+	cout << "Hello, world!" << endl;
+	system("PAUSE");
+	return 0;
+}
---
+++ Project1/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+using namespace std;
+
+#endif // MAIN_H
